0f219328eb0909d3b0aaa4490271506b9689035d
dirkarnez <smalldirkalex@gmail.com>
2024-03-28 17:35:46 +0800

    - wip

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/dirkarnez/go-mobile-playground
 
 go 1.21.0
+
+require golang.org/x/mobile v0.0.0-20240326195318-268e6c3a80d1
+
+require (
+	golang.org/x/exp/shiny v0.0.0-20230817173708-d852ddb80c63 // indirect
+	golang.org/x/image v0.15.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+)
--- local-build.sh
+++ local-build.sh
@@ -1,21 +1,26 @@
 export current_directory=$(PWD)
-
-
 export USERPROFILE=$(cygpath -u $USERPROFILE)
 
-
 export ANDROID_HOME=$USERPROFILE/Downloads/commandlinetools-win-9123335_latest
-
+export GOROOT=$USERPROFILE/Downloads/go1.21.0.windows-amd64/go
 export PATH=$ANDROID_HOME/cmdline-tools/bin/:$PATH
 export PATH=$ANDROID_HOME/emulator/:$PATH
 export PATH=$ANDROID_HOME/platform-tools/:$PATH
-export PATH=$USERPROFILE/Downloads/cmake-3.22.2-windows-x86_64/bin:$PATH
+export PATH=$GOROOT/bin:$PATH
+
 
 export ANDROID_NDK_HOME=$ANDROID_HOME/ndk-bundle
 export BUILD_TOOLS=$ANDROID_HOME/build-tools/27.0.2
 
 export JAVA_HOME=$USERPROFILE/Downloads/OpenJDK8U-jdk_x64_windows_hotspot_8u322b06/jdk8u322-b06
 export PATH=$JAVA_HOME/bin/:$PATH
+export GO111MODULE=on
+export GOPATH=$USERPROFILE/Downloads/gopath
+export GOSUMDB=sum.golang.google.cn
+export GOPROXY=proxy.golang.org
+
+# https://dl.google.com/android/repository/commandlinetools-win-9123335_latest.zip
+# https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u322-b06/OpenJDK8U-jdk_x64_windows_hotspot_8u322b06.zip
 
 # export ANDROID_NDK=$USERPROFILE/android-ndk-r25b-windows/android-ndk-r25b
 
@@ -50,5 +55,8 @@ sdkmanager.bat --sdk_root=$ANDROID_HOME --install "platform-tools" "platforms;an
 # $USERPROFILE/Downloads\commandlinetools-win-9123335_latest\ndk\25.1.8937393\toolchains\llvm\prebuilt\windows-x86_64\bin
 echo $ANDROID_NDK_HOME && \
 cd $current_directory && \
-$USERPROFILE/Downloads/gopath/bin/gomobile  build -target android
-read -p "sdasd"
+(
+    $USERPROFILE/Downloads/gopath/bin/gomobile init && \
+    $USERPROFILE/Downloads/gopath/bin/gomobile  build -target android && \
+    read -p "sdasd"
+) || read -p "not ok"
